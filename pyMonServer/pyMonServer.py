3f3d4b7ea92a030b7225ae0738abd48be9e6f937
VGol-Git <61864261+VGol-Git@users.noreply.github.com>
2023-10-02 21:49:22 +0300

    Server update
    - adding a Client Screenshot Implementation
    - adding a view of client activity in the format json

--- pyMonServer/pyMonServer.py
+++ pyMonServer/pyMonServer.py
@@ -1,36 +1,108 @@
-
 import socket
-import subprocess
-import os
+import threading
+import json
 
 # Хост и порт, на котором будет работать сервер
 HOST = '127.0.0.1'
-
 PORT = 12345
 
-def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind((HOST, PORT))
-        server_socket.listen()
+clients = {}  # Список подключенных клиентов
+def list_clients():
+    print("Подключенные клиенты:")
+    for i, (conn, addr) in enumerate(clients, start=1):
+        print(f"{i}. {conn}, {addr}")
 
-        print(f"Сервер слушает на {HOST}:{PORT}")
 
-        while True:
-            conn, addr = server_socket.accept()
-            with conn:
-                print(f"Подключение от {addr}")
-                data = conn.recv(1024)
+def receive_and_save_bmp(client_socket, file_name):
+    try:
+        with open(file_name, 'wb') as file:
+            # Принимаем данные заголовка BMP файла
+            bmp_header = client_socket.recv(54)
+            if len(bmp_header) != 54:
+                print("Ошибка: Не удалось получить заголовок BMP файла.")
+                return
+
+            file.write(bmp_header)
+
+            # Извлекаем информацию о размере файла из заголовка
+            file_size = int.from_bytes(bmp_header[2:6], byteorder='little')
+
+            # Принимаем и записываем остальные данные файла
+            remaining_data = file_size - 54
+            while remaining_data > 0:
+                data = client_socket.recv(min(1024, remaining_data))
                 if not data:
                     break
+                file.write(data)
+                remaining_data -= len(data)
 
-                command = data.decode()
+            print(f"Файл '{file_name}' успешно принят и сохранен.")
+    except Exception as e:
+        print(f"Произошла ошибка при приеме файла: {str(e)}")
+        
+# Первое распараллеливание: принимаем новых клиентов
+def accept_clients(server_socket):
+    while True:
+        # Получаем уникальный идентификатор клиента: IP-адрес и порт
+        client_socket, addr = server_socket.accept()
+        client_id = addr
+        # Сохраняем соединение клиента в словаре
+        clients[client_id] = client_socket
 
-                if command == "screenshot":
-                    conn.send("screenshot")
-                #if command == "delete":
-                #    conn.send("")
+      
+
+# Второе распараллеливание: отправляем данные клиентам
+def get_data():
+    while True:
+        command = input("Введите команду (screenshot [ip] [clientId], json [ip] [clientId], или view): ")
+        if command == "view":
+            list_clients()
+            continue
+
+        parts = command.split()
+        if len(parts) == 3:
+            try:
+                message = parts[0]
+                ip = parts[1]
+                client_id = int(parts[2]) 
+                if client_id >= 0 and len(message) > 0:
+                    clients[(ip, client_id)].send(message.encode())
+                    if message == 'screenshot':
+                        receive_and_save_bmp(clients[(ip, client_id)], "screenshot.bmp")
+                        
+
+                 # Принимаем JSON файл
+                    if message == 'json':
+                        json_data = clients[(ip, client_id)].recv(40960)  
+                        parsed_json = json.loads(json_data.decode('utf-8'))
+                        print("Принят JSON файл:")
+                        print(parsed_json)
+                       
+                        filename = "received_data.json"
+                        with open(filename, 'w') as file:
+                            json.dump(parsed_json, file)
+
                 else:
-                    conn.send("Неизвестная команда.")
+                    print("Неправильный данные клиента")
+            except ValueError:
+                print("Неправильный формат команды")
+            except json.JSONDecodeError:
+                print("Ошибка при разборе JSON файла")
+
+        else:
+            print("Неправильный формат команды")
+def main():
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((HOST, PORT))
+        server_socket.listen()
+        print(f"Сервер слушает на {HOST}:{PORT}")
+        accept_thread = threading.Thread(target=accept_clients, args=(server_socket,))
+        send_thread = threading.Thread(target=get_data, )
+
+        accept_thread.start()
+        send_thread.start()
+
+
 
 if __name__ == "__main__":
     main()
